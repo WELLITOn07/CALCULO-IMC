b51e8cea8781c797bb836ac4e9ebebe8b093687c
WELLITOn07 <w.elliton2010@hotmail.com>
2022-05-11 23:01:39 -0300

    att

--- js.js
+++ js.js
@@ -13,8 +13,9 @@ function resultado (evento) {
     const alturaHTML = document.getElementById ('altura');
     let altura = Number(alturaHTML.value);
     let IMC = peso / (altura * 2);
+
     if (peso == 0 || altura == 0) {
-        window.alert('Necessário adicionar os valores!')    
+        window.alert('Necessário adicionar os valores ou usar ponto (.) na altura, exp: 1.80')    
     } else if (IMC <= 18.5) {
         tela_resultado.innerHTML = `Seu IMC: ${IMC.toFixed(1)} </br> <span>MAGREZA</span>`
     } else if (IMC <= 24.9) {
@@ -23,7 +24,7 @@ function resultado (evento) {
         tela_resultado.innerHTML = `Seu IMC: ${IMC.toFixed(1)} </br> <span>SOBREPESO</span>`
     } else if (IMC <= 39.9) {
         tela_resultado.innerHTML = `Seu IMC: ${IMC.toFixed(1)} </br> <span>OBESIDADE</span>`
-    } else {
+    } else if (IMC >= 40 ) {
         tela_resultado.innerHTML = `Seu IMC: ${IMC.toFixed(1)} </br> <span>OBESIDADE-GRAVE</span>`
     }; 
     
